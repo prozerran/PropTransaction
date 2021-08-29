c3cde684139afa46eff6eeb85fd70dad95f62d99
prozerran <zaken520@gmail.com>
2021-08-29 16:48:44 +0800

    fixes

--- PropTransaction/ClientApp/src/App.js
+++ PropTransaction/ClientApp/src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
 import { Registration } from './components/Registration';
 import { Login } from './components/Login';
 import { PropertyMod } from './components/PropertyMod';
 import { PropertyView } from './components/PropertyView';
-import { Counter } from './components/Counter';
 
 import './custom.css'
 
@@ -17,9 +14,6 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-            <Route exact path='/' component={Home} />
-            <Route path='/counter' component={Counter} />
-            <Route path='/fetch-data' component={FetchData} />
             <Route path='/register-user' component={Registration} />
             <Route path='/login-user' component={Login} />
             <Route path='/property-mod' component={PropertyMod} />
